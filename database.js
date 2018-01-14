f036c42643826b6e65379238570e7851bd9c4fca
Blaz Blokar <bblokar@gmail.com>
2018-01-14 23:11:29 +0100

    nove funkcionalnosti

--- database.js
+++ database.js
@@ -1,53 +1,213 @@
-var s = 'sebita';
-
 const Sequelize = require('sequelize');
+const sleep = require('sleep');
 
-const sequelize = new Sequelize('db', null, null, {
-  dialect: 'sqlite',
-  storage: 'db.sqlite'
-});
+class DBConnection {
+    constructor(mode) {
 
-
-sequelize.authenticate().then( (err) => {
-	console.log("connection works");
-}, (err) => {
-	console.log("no it doesnt");
-});
-
-
-////////////////////////////////////////////////////////// MODELS ///////////////////////////////////////////////////////////
-
-const Deploy = sequelize.define('deploycontracts', {
-  id: { type: Sequelize.UUID, primaryKey: true },
-  contract: Sequelize.STRING,
-  address: Sequelize.STRING
-});
+        this.sequelize = new Sequelize('db', null, null, {
+            dialect: 'sqlite',
+            storage: 'db.sqlite',
+            omitNull: true,
+            define: {
+                timestamps: false
+            }
+        });
 
 
-const User = sequelize.define('users', {
-  id: { type: Sequelize.UUID, primaryKey: true },
-  username: Sequelize.STRING, 
-  password: Sequelize.STRING,
-  realname: Sequelize.STRING
-});
+        this.sequelize.authenticate().then((err) => {
+            console.log("connection works");
+        }, (err) => {
+            console.log("no it doesnt");
+        });
+
+        this.Deploy = this.sequelize.define('deploycontracts', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            contract: Sequelize.STRING,
+            address: Sequelize.STRING
+        });
+
+        this.User = this.sequelize.define('users', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                notNull: true
+            },
+            username: Sequelize.STRING,
+            password: Sequelize.STRING,
+            realname: Sequelize.STRING
+        });
+
+        this.Deploy.belongsTo(this.User);
 
 
-Deploy.belongsTo(User);
+        this.sequelize.sync({
+            force: mode
+        }).then((err) => {
+            console.log("it works!")
+        });
+
+    }
 
 
-sequelize.sync({ force: true }).then( (err) => { console.log("it works!") });
+    checkPass(req, username, password, done) {
+        console.log("::::::: " + username + " : " + password);
+        this.User.findOne({
+            where: {
+                "username": username
+            }
+        }).then(user => {
+            if (user === null) {
+                done(null, false, { message: 'Incorrect username.' });
+            } else {
+                if (user.password === password) {
+
+                    console.log("successfully logged in");
+                    done(null, user);
+                } else {
+                    //console.log("is not");
+                    done(null, false, { message: 'Incorrect password.' });
+                }
+            }
+        });
+    }
+
+
+    insertUser(uname, pass, rname) {
+        this.User.create({
+            username: uname,
+            password: pass,
+            realname: rname
+        })
+    }
+
+}
+
+if (require.main === module) {
+    const dbConnection = new DBConnection(false);
+    sleep.sleep(2);
+    dbConnection.insertUser("blaz", "pass", "blaz blokar");
+
+
+}
+
+module.exports = DBConnection;
+
+
+
+/*insertUsers(uname, pass, rname) {
+        this.User.create({
+            username: uname,
+            password: pass,
+            realname: rname
+        }).then(() => User.findOrCreate({
+            where: {
+                username: 'eva'
+            },
+            defaults: {
+                password: 'hoho'
+            }
+        })).spread((user) => {
+            console.log(user.get({
+                plain: true
+            }))
+            console.log(created)
+        });
+    }
+    //////////////////////////////////// INSERST DEPLOYCONTRACTS ////////////////////////////////////////////////////////////////////////////
+    insertDeploy(uname, cont, adr) {
+        var maxId = findMaxCont();
+        this.Deploy.create({
+            id: maxId,
+            users: uname,
+            contract: cont,
+            adress: adr
+        }).then(() => this.User.findOrCreate({
+            where: {
+                id: maxId
+            },
+            defaults: {
+                adress: adr
+            }
+        })).spread((user) => {
+            console.log(user.get({
+                plain: true
+            }))
+            console.log(created)
+        });
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////  FIND USER  //////////////////////////////////////////////////////////////////////////////////////
+    findUser(uname) {
+        this.sequelize.query('SELECT * FROM users WHERE username = ?', {
+            raw: true,
+            replacements: ['sebita']
+        }).then(projects => {
+            console.log(projects[0])
+        });
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////   FIND CONTRACTS   ///////////////////////////////////////////////////////////////////////////////////////////////////
+    findContracts(unames) {
+        this.sequelize.query('SELECT * FROM deploycontracts WHERE users = ?', {
+            raw: true,
+            replacements: [unames]
+        }).then(projects => {
+            console.log(projects[0])
+        });
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////// FIND MAX CONTRACT ///////////////////////////////////////////////////////////////////////////
+    findMaxCont() {
+        Deploy.max('id').then(max => {
+            console.log(max); // this will return 40
+            return max;
+        });
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////  DELETE USERS  /////////////////////////////////////////////////////////////////////////////////////////////
+    deleteUser(uname) {
+        User.destroy({
+            where: {
+                username: uname //this will be your id that you want to delete
+            }
+        }).then(function(rowDeleted) { // rowDeleted will return number of rows deleted
+            if (rowDeleted === 1) {
+                console.log('Deleted successfully');
+            }
+        }, function(err) {
+            console.log(err);
+        });
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////  DELETE DEPLOY CONTRACTS  ////////////////////////////////////////////////////////////////////////////////////////////////
+    deleteContracts(adr) {
+        Deploy.destroy({
+            where: {
+                adress: adr //this will be your id that you want to delete
+            }
+        }).then(function(rowDeleted) { // rowDeleted will return number of rows deleted
+            if (rowDeleted === 1) {
+                console.log('Deleted successfully');
+            }
+        }, function(err) {
+            console.log(err);
+        });
+    }
+}
 ///////////////////////////////////////////  USE FUNCTIONS  ////////////////////////////////////////////////////////////////////////////
-
-insertUsers('sebita','hahah','cualquiera');
 //insertDeploy('sebita','Quiero terminar de una vez por todas!!!!', 'gmail.com');
 //findUser('sebita');
 //findContracts('sebita');
 //deleteUser('eva');
 //deleteContracts('gmail.com');
-
 /* ORM 
 
-var User = sequelize.define('User', {
+var User = this.sequelize.define('User', {
 	username: Sequelize.STRING,
 	password: Sequelize.STRING,
 	fullname: Sequelize.STRING
@@ -58,115 +218,12 @@ sequelize.sync({ force: true }).then( (err) => { console.log("it works!") });
 
 sequelize.findAll(...)
 
-*/ 
-
-
+*/
 /* Raw */
 /*
 sequelize.query("SELECT * FROM deploycontracts", { type: sequelize.QueryTypes.SELECT }).then( users => {
 	console.log(users);
 });
 */
-
 //////////////////////////////////// INSERST USERS ////////////////////////////////////////////////////////////////////////////
-
-
-
-function insertUsers(uname,pass,rname){
-User.create({ username: uname, password: pass, realname: rname })
-  .then(() => User.findOrCreate({where: {username: 'eva'}, defaults: {password: 'hoho'}}))
-  .spread((user) => {
-    console.log(user.get({
-      plain: true
-    }))
-    console.log(created)
-  });
-}
-
-
-
-
-
-//////////////////////////////////// INSERST DEPLOYCONTRACTS ////////////////////////////////////////////////////////////////////////////
-
-
-function insertDeploy(uname,cont,adr){
-	var maxId = findMaxCont();
-	Deploy.create({ id: maxId, users: uname, contract: cont, adress: adr })
-	  .then(() => User.findOrCreate({where: {id: maxId}, defaults: {adress: adr}}))
-	  .spread((user) => {
-	    console.log(user.get({
-	      plain: true
-	    }))
-	    console.log(created)
-	  });
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////  FIND USER  //////////////////////////////////////////////////////////////////////////////////////
-
-function findUser(uname){ 
-	sequelize.query('SELECT * FROM users WHERE username = ?',{ raw: true, replacements: ['sebita']}).then(projects => {
-	    console.log(projects[0])
-	  });
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////   FIND CONTRACTS   ///////////////////////////////////////////////////////////////////////////////////////////////////
-
-function findContracts(unames){
-	sequelize.query('SELECT * FROM deploycontracts WHERE users = ?',{ raw: true, replacements: [unames]}).then(projects => {
-	    console.log(projects[0])
-	  });
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////// FIND MAX CONTRACT ///////////////////////////////////////////////////////////////////////////
-
-function findMaxCont(){
-	Deploy.max('id').then(max => {
-	  console.log(max);// this will return 40
-	  return max;
-	});
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////  DELETE USERS  /////////////////////////////////////////////////////////////////////////////////////////////
-
-
-function deleteUser(uname){
-	User.destroy({
-	   where: {
-	      username: uname //this will be your id that you want to delete
-	   }
-	}).then(function(rowDeleted){ // rowDeleted will return number of rows deleted
-	  if(rowDeleted === 1){
-	     console.log('Deleted successfully');
-	   }
-	}, function(err){
-	    console.log(err); 
-	});
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////  DELETE DEPLOY CONTRACTS  ////////////////////////////////////////////////////////////////////////////////////////////////
-
-function deleteContracts(adr){
-	Deploy.destroy({
-	   where: {
-	      adress: adr //this will be your id that you want to delete
-	   }
-	}).then(function(rowDeleted){ // rowDeleted will return number of rows deleted
-	  if(rowDeleted === 1){
-	     console.log('Deleted successfully');
-	   }
-	}, function(err){
-	    console.log(err); 
-	});
-}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- index.js
+++ index.js
@@ -1,23 +1,120 @@
 const crypto = require('crypto');
 const EthConnection = require('./api');
-const express = require('express'),
-	app = express(),
-	port = process.env.PORT || 3000;
+const DBConneection = require('./database');
+const flash = require('connect-flash');
 
-app.use(express.static("frontend"));
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
+const passport = require('passport');
+const LocalStrategy = require("passport-local").Strategy;
+var session = require('express-session');
+var bodyParser = require('body-parser')
 
 ethConnection = new EthConnection();
 currentRequests = {};
 
+dbConnection = new DBConneection(false);
+
+
+passport.use(new LocalStrategy({
+	  passReqToCallback : true
+
+	}, (req, username, password, done) => {
+
+  		dbConnection.checkPass(req, username, password, done);
+	}
+));
+
+ passport.serializeUser(function (user, done)
+    {
+    	console.log("serializing: " + user.username);
+        done(null, user.username);
+       // console.log(user);
+    });
+
+    passport.deserializeUser(function (user, done)
+    {
+        done(null, user.username);
+});
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use(session({
+    secret: 'ne maram šole',
+    resave: false, 
+    saveUninitialized: false
+}));
+
+app.set('views', __dirname + '/frontend');
+
+app.get('/login', (req, res) => {
+	res.end("sorry");
+});
+
+app.get('/wallet', (req, res) => {
+	console.log(req.session.passport.user);
+	res.end("nice");
+});
+
+app.post('/login', 
+  passport.authenticate('local', { successRedirect: '/wallet', failureRedirect: '/login', failureFlash: true })
+);
+
+
+
+// views
+
+app.get('/', function(req, res) {
+    sess = req.session;
+    if (sess.user) {
+        res.redirect('/wallet');
+    } else {
+        res.render('index.html');
+    }
+});
+
+app.get('/logout', function(req, res) {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    })
+});
+
+
+/*
+app.get('/wallet', function(req, res) {
+    sess = req.session;
+    if (sess.user) {
+        res.redirect('/wallet');
+    } else {
+        res.write('<h1>Please login first.</h1>');
+        res.end('<a href="+">Login</a>');
+    }
+});
+
+*/
+
+// API
+
 app.get('/api/deploy', (req, res) => {
-	let id = crypto.randomBytes(6).toString('hex');
-	res.send(id);
-	res.status(201).end();
-	currentRequests[id] = null;
-
-
-
-	/* query v stilu:
+    let id = crypto.randomBytes(6).toString('hex');
+    res.send(id);
+    res.status(201).end();
+    currentRequests[id] = null;
+    /* query v stilu:
 
 	{
 		privateKey: "0x321123adssaddsa",
@@ -30,29 +127,23 @@ app.get('/api/deploy', (req, res) => {
 	ethConnection.deployContract(req.query.privateKey, req.query.contractName, req.query.params).then(function(res) {
     	currentRequests[id] = res;	
 	}); */
-
-	ethConnection.deployContract("0xca8b218de3ae8cba66fc3fd81d80d9bda9fead4f85d49ae7baa8a9b484da0e1c", "Zepnina", {
-		"receiver": "0x874b54a8bd152966d63f706bae1ffeb0411921e5"
-	}).then(function(res) {
-    	currentRequests[id] = res;	
-	});	
+    ethConnection.deployContract("0xca8b218de3ae8cba66fc3fd81d80d9bda9fead4f85d49ae7baa8a9b484da0e1c", "Zepnina", {
+        "receiver": "0x874b54a8bd152966d63f706bae1ffeb0411921e5"
+    }).then(function(res) {
+        currentRequests[id] = res;
+    });
 });
 
 app.get('/api/status/:id', (req, res) => {
-	console.log("index.js :: got request with id " + req.params.id);
-	if (currentRequests[req.params.id] != null) {
-		res.send(currentRequests[req.params.id]);
-	} else {
-		res.send("null");
-	}
-	res.status(201).end();
+    console.log("index.js :: got request with id " + req.params.id);
+    if (currentRequests[req.params.id] != null) {
+        res.send(currentRequests[req.params.id]);
+    } else {
+        res.send("null");
+    }
+    res.status(201).end();
 });
 
 app.listen(port, function() {
-	console.log("listening on localhost:3000");
+    console.log("listening on localhost:3000");
 });
-
-
-
-
-
